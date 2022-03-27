6a6e2c4da0b48c7c6f1b90e32e0420e5b80764e3
Precious-gift <falayeprecious@gmail.com>
2022-03-27 15:35:44 +0100

    UI UPDATE

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ togle.onclick = function() {
     document.getElementById("togle").classList.toggle("dtogle");
     if(document.getElementById("togle").classList.contains('dtogle')){
         document.body.style.backgroundColor = 'hsl(230, 17%, 14%)';
-        change.style.cssText = 'background-color: hsl(230, 17%, 14%); margin-left: 5px;';    
+        change.style.cssText = 'background-color: hsl(230, 17%, 14%); margin-left: 5px;';
+        document.body.getElementsByClassName('container').classList.add('dcontainer');    
     }else {
         document.body.style.backgroundColor = 'white';
-        change.style.cssText = 'background-color: hsl(225, 100%, 98%); margin-right: 5px;';
+        change.style.cssText = 'background-color: hsl(225, 100%, 98%); margin-right: 5px;';    
     }
 }
 
