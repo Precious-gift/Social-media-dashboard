39b451f44886045d776d540cdc6f68bead936d71
Precious-gift <falayeprecious@gmail.com>
2022-03-27 14:48:10 +0100

    UI UPDATE

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let dchange = document.getElementsByClassName('dtogle');
 togle.onclick = function() {
     document.getElementById("togle").classList.toggle("dtogle");
     if(document.getElementById("togle").classList.contains('dtogle')){
-        //change.attributes('indicator').removeNameItem;
-        //change.classList.toggle('dindicator');
         document.body.style.backgroundColor = 'hsl(230, 17%, 14%)';
         change.style.cssText = 'background-color: hsl(230, 17%, 14%); margin-left: 5px;';    
     }else {
